173a33e84533e9698ee3c847d10ea9a3a76b1728
tom-englert <mail@tom-englert.de>
2018-08-11 19:05:50 +0200

    Move SystemReferences extension methods to FodyTools

--- AutoProperties.Fody/SystemReferences.cs
+++ AutoProperties.Fody/SystemReferences.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
 
 using Fody;
+
+using FodyTools;
 
 using JetBrains.Annotations;
 
@@ -35,66 +35,5 @@ namespace AutoProperties.Fody
 
         [CanBeNull]
         public MethodReference GetPropertyInfo { get; }
-    }
-
-    static class ext
-    {
-        public static MethodReference ImportMethod<TResult>([NotNull] this BaseModuleWeaver weaver, [NotNull] Expression<Func<TResult>> expression)
-        {
-            GetMethodInfo(expression, out var methodName, out var declaringTypeName, out var argumentTypeNames);
-
-            var typeDefinition = weaver.FindType(declaringTypeName);
-
-            try
-            {
-                var method = typeDefinition.Methods
-                    .Single(m => (m.Name == methodName + "x") && m.Parameters.Select(p => p.ParameterType.Name).SequenceEqual(argumentTypeNames));
-
-                return weaver.ModuleDefinition.ImportReference(method);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw new InvalidOperationException($"Method {methodName} does not exist on type {declaringTypeName}", ex);
-            }
-        }
-
-        public static MethodReference TryImportMethod<TResult>([NotNull] this BaseModuleWeaver weaver, [NotNull] Expression<Func<TResult>> expression)
-        {
-            GetMethodInfo(expression, out var methodName, out var declaringTypeName, out var argumentTypeNames);
-
-            if (!weaver.TryFindType(declaringTypeName, out var typeDefinition))
-                return null;
-
-            var method = typeDefinition.Methods
-                .FirstOrDefault(m => m.Name == methodName && m.Parameters.Select(p => p.ParameterType.Name).SequenceEqual(argumentTypeNames));
-
-            if (method == null)
-                return null;
-
-            return weaver.ModuleDefinition.ImportReference(method);
-        }
-
-        public static TypeReference ImportType<T>([NotNull] this BaseModuleWeaver weaver)
-        {
-            return weaver.ModuleDefinition.ImportReference(weaver.FindType(typeof(T).Name));
-        }
-
-        public static TypeReference TryImportType<T>([NotNull] this BaseModuleWeaver weaver)
-        {
-            if (!weaver.TryFindType(typeof(T).Name, out var typeDefinition))
-                return null;
-
-            return weaver.ModuleDefinition.ImportReference(typeDefinition);
-        }
-
-        private static void GetMethodInfo<TResult>(Expression<Func<TResult>> expression, out string methodName, out string declaringTypeName, out string[] argumentTypeNames)
-        {
-            if (!(expression.Body is MethodCallExpression methodCall))
-                throw new ArgumentException("Only method call expression is supported.", nameof(expression));
-
-            methodName = methodCall.Method.Name;
-            declaringTypeName = methodCall.Method.DeclaringType.Name;
-            argumentTypeNames = methodCall.Arguments.Select(a => a.Type.Name).ToArray();
-        }
     }
 }
